87c545451ba7467e1ad1c241614dbdffb9e59c74
ICY-glitch <82642948+ICY-glitch@users.noreply.github.com>
2025-01-12 16:28:56 +0100

    jelentkezolap finito

--- jelentkezes.php
+++ jelentkezes.php
@@ -1,1 +1,25 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nev = $_POST['nev'];
+    $ev = $_POST['ev'];
+    $lakcim = $_POST['lakcim'];
+    $telefon = $_POST['telefon'];
+    $email = $_POST['email'];
+    $nem = $_POST['nem'];
+
+    $tapasztalat = $_POST['tapasztalat'];
+    $iskola = $_POST['iskola'];
+    $nyelvtudas = $_POST['nyelvtudas'];
+
+    $gyakornok = $_POST['gyakornok'];
+    $adminisztrator = $_POST['adminisztrator'];
+    $alkfejleszto = $_POST['alkfejleszto'];
+    $hkarbantarto = $_POST['hkarbantarto'];
+    $tanito = $_POST['tanito'];
+
+    $nyelvek = $_POST['nyelvek'];
+
+
+
+
+}
